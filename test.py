4109d6022d4ff3bf24ec8942adb9f8aa84077a5f
Joel Castro <112850905+Joel-CA@users.noreply.github.com>
2025-02-28 05:03:16 -0800

    Initial commit

---
+++ test.py
@@ -0,0 +1,4 @@
+from OpenGL.GL import glGetUniformLocation as a
+from OpenGL.GL import glActiveTexture as b
+OpenGL.lazywrapper.lazy( 'glGetUniformLocation' )
+OpenGL.platform.baseplatform.glActiveTexture object at 0x000002845FD4C550>
